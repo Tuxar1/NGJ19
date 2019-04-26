fc5bee7f08892d3679a6669ce20bb60db13a635a
Frederik Knudsen <fknudsen@gmail.com>
2019-04-26 21:48:59 +0200

    merge

--- ArenaGOGO2/Assets/Scripts/playerMove.cs
+++ ArenaGOGO2/Assets/Scripts/playerMove.cs
@@ -9,7 +9,11 @@ public class playerMove : MonoBehaviour
 	public float JumpVelocity = 1000f;
     public Vector3 FacingDirection = new Vector3(1, 0, 0);
 
-	public Rigidbody2D rigidbody;
+	public Rigidbody2D Rigidbody;
+	public Transform GroundCheck;
+
+	private bool grounded = false;
+	private bool jump = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,26 +21,35 @@ public class playerMove : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+	void Update()
+	{
+		grounded = Physics2D.Linecast(transform.position, new Vector2(transform.position.x, transform.position.y - 0.75f), 1 << LayerMask.NameToLayer("Ground"));
+		
+		if (grounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			jump = true;
+		}
+	}
+
+	void FixedUpdate()
     {
 		var horizontalMovement = Input.GetAxis("Horizontal");
 
-		if (horizontalMovement * rigidbody.velocity.x < MaxXVelocity)
+		if (horizontalMovement * Rigidbody.velocity.x < MaxXVelocity)
 		{
             if (horizontalMovement != 0)
                 FacingDirection.x = horizontalMovement > 0 ? 1 : -1;
-            print(FacingDirection);
 			rigidbody.AddForce(Vector2.right * horizontalMovement * HorizontalForce);
 		}
-		if (Mathf.Abs(rigidbody.velocity.x) > MaxXVelocity)
+		if (Mathf.Abs(Rigidbody.velocity.x) > MaxXVelocity)
 		{
-			rigidbody.velocity = new Vector2(Mathf.Sign(rigidbody.velocity.x) * MaxXVelocity, rigidbody.velocity.y);
+			Rigidbody.velocity = new Vector2(Mathf.Sign(Rigidbody.velocity.x) * MaxXVelocity, Rigidbody.velocity.y);
 		}
 
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (jump)
 		{
-			rigidbody.AddForce(new Vector2(0, JumpVelocity));
+			jump = false;
+			Rigidbody.AddForce(new Vector2(0, JumpVelocity));
 		}
     }
 }
