85718e2d4d2e2708b55fd241c6cd859a6a4678f2
Andreas <adolsen@gmail.com>
2019-04-27 12:15:14 +0200

    Andreas test scene

---
+++ LeMans/Assets/Scripts/RoadGeneration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyRoads3Dv3;
+
+public class RoadGeneration : MonoBehaviour
+{
+    private ERRoadNetwork roadNetwork;
+    private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        roadNetwork = new ERRoadNetwork();
+
+        Vector3[] markers = { Vector3.zero};
+        
+        roadNetwork.AddInititialMarkers(new ERRoad(), markers);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
