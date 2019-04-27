4fee2fae806f63f11c2bdff194b947c7ba36f1b0
Andreas <adolsen@gmail.com>
2019-04-27 18:13:17 +0200

    Auto stash before merge of "master" and "origin/master"

--- LeMans/Assets/Scripts/RoadGeneration.cs
+++ LeMans/Assets/Scripts/RoadGeneration.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class RoadGeneration : MonoBehaviour
 {
     public GameObject Finish;
+    public GameObject Player1;
+    public GameObject Player2;
     public int Scale = 10;
 
-    private static int sizeX = 100;
-    private static int sizeY = 100;
+    private static int sizeX = 1000;
+    private static int sizeY = 1000;
     private bool[,] map = new bool[sizeX, sizeY];
 
     public Vector2 StartPosition = Vector2.zero;
@@ -16,9 +18,12 @@ public class RoadGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         GameObject creator = new GameObject();
         StartPosition = new Vector2(sizeX / 2, sizeX / 2);
         creator.transform.position = new Vector3(StartPosition.x, 0, StartPosition.y);
+
+
         for (int x = 0; x < sizeX; x++)
         {
             for (int y = 0; y < sizeY; y++)
@@ -27,12 +32,12 @@ public class RoadGeneration : MonoBehaviour
             }
         }
         int currentDirection = 1;
-        map[(int)StartPosition.x, (int)StartPosition.y] = true;
-        for (int i = 0; i < 100; i++)
+        try
         {
 
-            try
+            for (int i = 0; i < 100; i++)
             {
+            
                 if (map[(int)creator.transform.position.x, (int)creator.transform.position.z] == false)
                 {
                     map[(int)creator.transform.position.x, (int)creator.transform.position.z] = true;
@@ -59,12 +64,29 @@ public class RoadGeneration : MonoBehaviour
 
                 currentDirection = Random.Range(0, 10);
             }
-            catch (System.Exception)
-            {
-                var fin = Instantiate(Finish, new Vector3(creator.transform.position.x, 0, creator.transform.position.z),Quaternion.identity);
-                fin.transform.rotation = creator.transform.rotation;
+            var fin = Instantiate(Finish, new Vector3(creator.transform.position.x, 0, creator.transform.position.z), Quaternion.identity);
+            fin.transform.rotation = creator.transform.rotation;
 
-            }
+            var player1 = Instantiate(Player1, new Vector3(StartPosition.x, 1, StartPosition.y), Quaternion.identity);
+            player1.transform.LookAt(fin.transform.position);
+            player1.transform.Translate(player1.transform.right);
+
+            var player2 = Instantiate(Player2, new Vector3(StartPosition.x, 1, StartPosition.y), Quaternion.identity);
+            player2.transform.LookAt(fin.transform.position);
+            player2.transform.Translate(-player2.transform.right);
+        }
+        catch (System.Exception)
+        {
+            var fin = Instantiate(Finish, new Vector3(creator.transform.position.x, 0, creator.transform.position.z), Quaternion.identity);
+            fin.transform.rotation = creator.transform.rotation;
+
+            var player1 = Instantiate(Player1, new Vector3(StartPosition.x, 1, StartPosition.y), Quaternion.identity);
+            player1.transform.LookAt(fin.transform.position);
+            player1.transform.Translate(player1.transform.right);
+
+            var player2 = Instantiate(Player2, new Vector3(StartPosition.x, 1, StartPosition.y), Quaternion.identity);
+            player2.transform.LookAt(fin.transform.position);
+            player2.transform.Translate(-player2.transform.right);
         }
     }
 
