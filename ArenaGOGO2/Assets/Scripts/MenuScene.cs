a8718f20aa8c0fdd282879f03a5b2fd6cffebe40
Frederik Knudsen <fknudsen@gmail.com>
2019-04-27 12:46:56 +0200

    MenuScene + controller input support

---
+++ ArenaGOGO2/Assets/Scripts/MenuScene.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+	private class JoinedPlayer
+	{
+		public InputType InputType;
+		public int InputID;
+		public Color SelectedColor;
+	}
+
+	public Color[] SelectableColors =
+	{
+		Color.red,
+		Color.blue,
+		Color.green,
+		Color.yellow,
+	};
+
+	public MenuPlayer[] menuPlayers;
+	public GameObject StartText;
+
+	private const int MaxPlayers = 4;
+	private const int JoyStickInputSetup = 4;
+	private const string BaseJoyName = "Joy";
+	private const string HorizontalName = "X";
+	private const string JumpName = "Jump";
+	private const string AttackName = "Attack";
+
+	private const int KeyboardInputCount = 2;
+
+	private JoinedPlayer[] joinedPlayers;
+
+	private bool started = false;
+
+	private enum InputType {
+		Keyboard,
+		Joystick
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		joinedPlayers = new JoinedPlayer[MaxPlayers];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		ReadJoinLeaveInput();
+		ReadStartGame();
+    }
+
+	void ReadJoinLeaveInput()
+	{
+		if (started)
+		{
+			return;
+		}
+		for (int i = 1; i < JoyStickInputSetup + 1; i++)
+		{
+			if (Input.GetButtonDown(BaseJoyName + i + JumpName))
+			{
+				AddRemovePlayer(i, InputType.Joystick);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			AddRemovePlayer(1, InputType.Keyboard);
+		}
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			AddRemovePlayer(2, InputType.Keyboard);
+		}
+	}
+
+	void ReadStartGame()
+	{
+		if (joinedPlayers.Any(jp => jp != null))
+		{
+			StartText.SetActive(true);
+			if (Input.GetButtonDown("Start"))
+			{
+				started = true;
+				StartGame();
+			}
+		}
+		else
+		{
+			StartText.SetActive(false);
+		}
+	}
+
+	void AddRemovePlayer(int inputID, InputType inputType)
+	{
+		int? canJoinID = null;
+		for (int i = 0; i < joinedPlayers.Length; i++)
+		{
+			var player = joinedPlayers[i];
+			if (player == null)
+			{
+				if (canJoinID == null)
+				{
+					canJoinID = i;
+				}
+				else
+				{
+					canJoinID = Mathf.Min(i, canJoinID.Value);
+				}
+				continue;
+			}
+			if (player.InputType == inputType && player.InputID == inputID)
+			{
+				joinedPlayers[i] = null;
+				menuPlayers[i].DeSelected();
+				return;
+			}
+		}
+		if (canJoinID.HasValue)
+		{
+			joinedPlayers[canJoinID.Value] = new JoinedPlayer
+			{
+				InputID = inputID,
+				InputType = inputType,
+				SelectedColor = GetUnusedColor(),
+			};
+			menuPlayers[canJoinID.Value].Selected(joinedPlayers[canJoinID.Value].SelectedColor);
+		}
+	}
+
+	private Color GetUnusedColor()
+	{
+		var noPlayers = joinedPlayers.All(jp => jp == null);
+		if (noPlayers)
+		{
+			return SelectableColors[0];
+		}
+		foreach (var color in SelectableColors)
+		{
+			if (joinedPlayers.All(jp => jp == null || jp.SelectedColor != color))
+			{
+				return color;
+			}
+		}
+		throw new System.Exception("Could not select a color :O");
+	}
+
+	private void StartGame()
+	{
+		PlayerSetup.ResetPlayers();
+		for (int i = 0; i < joinedPlayers.Length; i++)
+		{
+			var player = joinedPlayers[i];
+			if (player == null)
+			{
+				continue;
+			}
+			if (player.InputType == InputType.Joystick)
+			{
+				PlayerSetup.SetUpJoystickPlayer(player.InputID, i, BaseJoyName + i + HorizontalName, BaseJoyName + i + JumpName, BaseJoyName + i + AttackName, player.SelectedColor);
+			}
+			else
+			{
+				var jumpKey = player.InputID == 1 ? KeyCode.UpArrow : KeyCode.W;
+				var attackKey = player.InputID == 1 ? KeyCode.RightControl : KeyCode.LeftControl;
+				PlayerSetup.SetUpKeyboardPlayer(player.InputID, i, "Key" + i + HorizontalName, jumpKey, attackKey, player.SelectedColor);
+			}
+		}
+		SceneManager.LoadScene(1);
+	}
+}
--- ArenaGOGO2/Assets/Scripts/PlayerSetup.cs
+++ ArenaGOGO2/Assets/Scripts/PlayerSetup.cs
@@ -7,26 +7,61 @@ public class PlayerSetup
     private struct PlayerData
 	{
 		public KeyCode JumpKey;
+		public string JumpButton;
 		public string HorizontalAxisName;
 		public KeyCode AttackKey;
+		public string AttackButton;
 		public Color PlayerColor;
+		public int InputID;
+		public bool Joystick;
 	}
 
 	private static Dictionary<int, PlayerData> playerID2InputData = new Dictionary<int, PlayerData>();
+
+	public static void ResetPlayers()
+	{
+		playerID2InputData = new Dictionary<int, PlayerData>();
+	}
+
+	public static void SetUpJoystickPlayer(int inputID, int playerID, string horizontalName, string jumpButtonName, string attackButtonName, Color color)
+	{
+		playerID2InputData[playerID] = new PlayerData
+		{
+			AttackButton = attackButtonName,
+			HorizontalAxisName = horizontalName,
+			JumpButton = jumpButtonName,
+			InputID = inputID,
+			PlayerColor = color,
+			Joystick = true,
+		};
+	}
+
+	public static void SetUpKeyboardPlayer(int inputID, int playerID, string horizontalName, KeyCode jumpButtonCode, KeyCode attackButtonCode, Color color)
+	{
+		playerID2InputData[playerID] = new PlayerData
+		{
+			AttackKey = attackButtonCode,
+			HorizontalAxisName = horizontalName,
+			JumpKey = jumpButtonCode,
+			InputID = inputID,
+			PlayerColor = color,
+			Joystick = false,
+		};
+	}
 
 	public static void SetUpDefaultIfNoData()
 	{
 		var player1Input = new PlayerData
 		{
 			JumpKey = KeyCode.UpArrow,
-			HorizontalAxisName = "Horizontal",
+			HorizontalAxisName = "Key1X",
 			AttackKey = KeyCode.RightControl,
 			PlayerColor = Color.blue,
 		};
 		var player2Input = new PlayerData
 		{
 			JumpKey = KeyCode.W,
-			HorizontalAxisName = "Horizontal2",
+			HorizontalAxisName = "Key2X",
 			AttackKey = KeyCode.LeftControl,
 			PlayerColor = Color.red,
 		};
@@ -34,23 +69,40 @@ public class PlayerSetup
 		playerID2InputData[1] = player2Input;
 	}
 
-	public static KeyCode GetJumpKeyForPlayer(int playerID)
-	{
-		return playerID2InputData[playerID].JumpKey;
-	}
-
-	public static string GetAxisNameForPlayer(int playerID)
-	{
-		return playerID2InputData[playerID].HorizontalAxisName;
-	}
-
-	public static KeyCode GetAttackKeyForPlayer(int playerID)
-	{
-		return playerID2InputData[playerID].AttackKey;
-	}
-
 	public static Color GetColorFromPlayerID(int playerID)
 	{
 		return playerID2InputData[playerID].PlayerColor;
+	}
+
+	public static bool GetPlayerJumpedForPlayerID(int playerID)
+	{
+		var pd = playerID2InputData[playerID];
+		if (pd.Joystick)
+		{
+			return Input.GetButtonDown(pd.JumpButton);
+		}
+		else
+		{
+			return Input.GetKeyDown(pd.JumpKey);
+		}
+	}
+
+	public static bool GetPlayerAttackedForPlayerID(int playerID)
+	{
+		var pd = playerID2InputData[playerID];
+		if (pd.Joystick)
+		{
+			return Input.GetButtonDown(pd.AttackButton);
+		}
+		else
+		{
+			return Input.GetKeyDown(pd.AttackKey);
+		}
+	}
+
+	public static float GetPlayerAxisForPlayerID(int playerID)
+	{
+		var pd = playerID2InputData[playerID];
+		return Input.GetAxis(pd.HorizontalAxisName);
 	}
 }
