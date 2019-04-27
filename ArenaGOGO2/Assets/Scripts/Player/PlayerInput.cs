293ef5cfd71e0f4d2f90e31bc801a1e1103deffa
Frederik Knudsen <fknudsen@gmail.com>
2019-04-27 10:44:33 +0200

    Created PlayerSetup.cs

--- ArenaGOGO2/Assets/Scripts/Player/PlayerInput.cs
+++ ArenaGOGO2/Assets/Scripts/Player/PlayerInput.cs
@@ -11,62 +11,22 @@ public class PlayerInput : MonoBehaviour
 	public Action<float> XAxis;
 	public Action<bool> AttackClicked;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	void Start()
+	{
+		GetComponent<SpriteRenderer>().color = PlayerSetup.GetColorFromPlayerID(PlayerID);
+	}
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
-        if (Input.GetKeyDown(getJumpKeyForPlayer(PlayerID)))
+        if (Input.GetKeyDown(PlayerSetup.GetJumpKeyForPlayer(PlayerID)))
 		{
 			JumpChanged(true);
 		}
-		XAxis(Input.GetAxis(getAxisNameForPlayer(PlayerID)));
-		if (Input.GetKeyDown(getAttackKeyForPlayer(PlayerID)))
+		XAxis(Input.GetAxis(PlayerSetup.GetAxisNameForPlayer(PlayerID)));
+		if (Input.GetKeyDown(PlayerSetup.GetAttackKeyForPlayer(PlayerID)))
 		{
 			AttackClicked(true);
 		}
-	}
-
-	private static KeyCode getJumpKeyForPlayer(int playerID)
-	{
-		if (playerID == 0)
-		{
-			return KeyCode.UpArrow;
-		}
-		else if (playerID == 1)
-		{
-			return KeyCode.W;
-		}
-		return KeyCode.Space;
-	}
-
-	private static string getAxisNameForPlayer(int playerID)
-	{
-		if (playerID == 0)
-		{
-			return "Horizontal";
-		}
-		else if (playerID == 1)
-		{
-			return "Horizontal2";
-		}
-		return "";
-	}
-
-	private static KeyCode getAttackKeyForPlayer(int playerID)
-	{
-		if (playerID == 0)
-		{
-			return KeyCode.RightControl;
-		}
-		else if (playerID == 1)
-		{
-			return KeyCode.LeftControl;
-		}
-		return KeyCode.Space;
 	}
 }
---
+++ ArenaGOGO2/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetup
+{
+    private struct PlayerData
+	{
+		public KeyCode JumpKey;
+		public string HorizontalAxisName;
+		public KeyCode AttackKey;
+		public Color PlayerColor;
+	}
+
+	private static Dictionary<int, PlayerData> playerID2InputData = new Dictionary<int, PlayerData>();
+
+	public static void SetUpDefaultIfNoData()
+	{
+		var player1Input = new PlayerData
+		{
+			JumpKey = KeyCode.UpArrow,
+			HorizontalAxisName = "Horizontal",
+			AttackKey = KeyCode.RightControl,
+			PlayerColor = Color.blue,
+		};
+		var player2Input = new PlayerData
+		{
+			JumpKey = KeyCode.W,
+			HorizontalAxisName = "Horizontal2",
+			AttackKey = KeyCode.LeftControl,
+			PlayerColor = Color.red,
+		};
+		playerID2InputData[0] = player1Input;
+		playerID2InputData[1] = player2Input;
+	}
+
+	public static KeyCode GetJumpKeyForPlayer(int playerID)
+	{
+		return playerID2InputData[playerID].JumpKey;
+	}
+
+	public static string GetAxisNameForPlayer(int playerID)
+	{
+		return playerID2InputData[playerID].HorizontalAxisName;
+	}
+
+	public static KeyCode GetAttackKeyForPlayer(int playerID)
+	{
+		return playerID2InputData[playerID].AttackKey;
+	}
+
+	public static Color GetColorFromPlayerID(int playerID)
+	{
+		return playerID2InputData[playerID].PlayerColor;
+	}
+}
